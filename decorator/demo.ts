8d1c6ff31c4d6d09265786e70f2778bda9728bc2
kiddyphan <kiddyphan@KiddyPhans-MacBook-Pro.local>
2018-09-05 14:56:11 +0700

    Update decorator pattern

--- decorator/demo.ts
+++ decorator/demo.ts
@@ -4,6 +4,10 @@ import { Breakfast } from "./breakfast";
 import { Lunch } from "./lunch";
 import { Room } from "./booking";
 
+
+/*
+    Decorator pattern lets you dynamically change the behavior of an object at run time by wrapping them in an object of a decorator class.
+*/
 export class DecoratorDemo {
     constructor() {
         let booking = new Wifi(new Breakfast(new Lunch(new Room(404))));
